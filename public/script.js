e0e9c2a7694cb18f92f72c5b06cc3c0fd5fc4daf
Palkoo1983 <paulsdiamond@gmail.com>
2025-10-11 12:04:37 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -3,8 +3,13 @@
   if ('scrollRestoration' in history) {
     history.scrollRestoration = 'manual';
   }
-  // azonnali (nem "smooth") felgörgetés a tetejére
-  window.scrollTo(0, 0);
+  // WebView-ban hagyjuk békén a pozíciót, mert ez villanást hozhat
+  var ua = navigator.userAgent || "";
+  var isAndroidWV = /\bwv\b/i.test(ua);
+  var isIOSWV = !!(window.webkit && window.webkit.messageHandlers) && !/Safari/i.test(ua);
+  if (!(isAndroidWV || isIOSWV)) {
+    window.scrollTo(0, 0);
+  }
 })();
 // Samsung Internet detektálás – biztosan lefut
 (function () {
